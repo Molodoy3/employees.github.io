25c0fd0fa41a28c80eea99919721227a34a6c778
Molodoy3 <artem.nikonov.2005@bk.ru>
2023-03-16 20:07:09 +0500

    ready

---
+++ src/js/app.js
@@ -0,0 +1,39 @@
+"use strict";
+//?Импортирование Свайпера
+//import Swiper, { Navigation, Pagination } from 'swiper'; //импорт свайпера
+//?Делегирование события клик
+//import delegationClick from './modules/script.js';
+//?Функция определения мобильного устройства
+//import { isMobile } from "./modules/functions";
+//?Импортирование плавного скролла
+//import "./modules/smoothScroll.js"
+
+window.addEventListener("load", windowLoad);
+function windowLoad() {
+
+
+
+    //?Создание плавного скролла
+/*     SmoothScroll({
+        // Время скролла 400 = 0.4 секунды
+        animationTime: 600,
+        // Размер шага в пикселях
+        stepSize: 75,
+        // Дополнительные настройки:
+        // Ускорение
+        accelerationDelta: 30,
+        // Максимальное ускорение
+        accelerationMax: 2,
+        // Поддержка клавиатуры
+        keyboardSupport: true,
+        // Шаг скролла стрелками на клавиатуре в пикселях
+        arrowScroll: 50,
+        // Pulse (less tweakable)
+        // ratio of "tail" to "acceleration"
+        pulseAlgorithm: true,
+        pulseScale: 4,
+        pulseNormalize: 1,
+        // Поддержка тачпада
+        touchpadSupport: true,
+    }); */
+}
